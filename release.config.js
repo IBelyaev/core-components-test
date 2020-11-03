be972daaf8b8b4ff98f8b3c7a173aeef8dea789f
Igor Belyaev <IYBelyaev@alfabank.ru>
2020-11-04 00:36:32 +0300

    fix(root): some CI rules

--- release.config.js
+++ release.config.js
@@ -3,13 +3,15 @@ module.exports = {
         '@semantic-release/commit-analyzer',
         '@semantic-release/release-notes-generator',
         '@semantic-release/changelog',
-        [
-            '@semantic-release/npm',
-            {
-                pkgRoot: './dist',
-                npmPublish: false,
-            },
-        ],
+        /*
+         * [
+         *     '@semantic-release/npm',
+         *     {
+         *         pkgRoot: './dist',
+         *         npmPublish: false,
+         *     },
+         * ],
+         */
         [
             '@semantic-release/npm',
             { npmPublish: false },
