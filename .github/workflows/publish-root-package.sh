9b2281452cb709f08de0241b29b79aea3d67c126
Igor Belyaev <IYBelyaev@alfabank.ru>
2020-11-16 18:51:55 +0300

    fix(root): changed yml

--- .github/workflows/publish-root-package.sh
+++ .github/workflows/publish-root-package.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-yarn publish dist --new-version echo `git describe --tags $(git rev-list --tags --max-count=1)` && sed -e "s/^v//" --no-git-tag-version
+yarn publish dist --new-version echo `git describe --tags $(git rev-list --tags --max-count=1)` | sed -e "s/^v//" --no-git-tag-version
