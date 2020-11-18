d0a4acf538463c38268daee14f91865bb19c78be
Igor Belyaev <IYBelyaev@alfabank.ru>
2020-11-18 10:06:15 +0300

    feat: adds some script

--- .github/workflows/publish-root-package.sh
+++ .github/workflows/publish-root-package.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-yarn publish dist --new-version echo `git describe --tags $(git rev-list --tags --max-count=1)` | sed -e "s/^v//"
+current_version=`git describe --tags $(git rev-list --tags --max-count=1) | sed -e "s/^v//"`
+
+yarn publish dist --no-git-tag-version --new-version $current_version
