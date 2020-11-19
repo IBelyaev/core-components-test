2726d17c4005c7d3ebdd1fa365887db65afa2415
Igor Belyaev <IYBelyaev@alfabank.ru>
2020-11-19 11:03:48 +0300

    feat(core-components-button-test): olololo

--- packages/button-test/src/Component.tsx
+++ packages/button-test/src/Component.tsx
@@ -9,7 +9,7 @@ import styles from './index.module.css';
 
 export type ComponentProps = {
     /**
-     * Тип кнопкиS
+     * Тип кнопки
      */
     view?: 'primary' | 'secondary' | 'outlined' | 'filled' | 'link' | 'ghost';
 
