03bb74c197a6e46642a3c8ef3bd881bec394a0f4
Igor Belyaev <IYBelyaev@alfabank.ru>
2020-12-15 20:31:20 +0300

    fix: olololo

---
+++ packages/release.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    plugins: [
+        '@semantic-release/commit-analyzer',
+        '@semantic-release/release-notes-generator',
+        '@semantic-release/changelog',
+        ['@semantic-release/npm', { pkgRoot: './' }],
+        ['@semantic-release/npm', { npmPublish: false }],
+        '@semantic-release/git',
+        '@semantic-release/github',
+    ],
+    branches: ['master'],
+    repositoryUrl: 'https://github.com/IBelyaev/core-components-test',
+};
