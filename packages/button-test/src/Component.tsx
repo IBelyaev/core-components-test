7851e2572b42c89a1fb0c962a7cc59f90ef97638
Dmitry Savkin <dmitrsavk@yandex.ru>
2021-01-14 11:44:44 +0300

    feat: major (#134)
    BREAKING CHANGE: breaking

--- packages/button-test/src/Component.tsx
+++ packages/button-test/src/Component.tsx
@@ -124,8 +124,6 @@ export const Button = React.forwardRef<HTMLAnchorElement | HTMLButtonElement, Bu
             HTMLButtonElement
         >;
 
-        // comment
-
         return (
             // eslint-disable-next-line react/button-has-type
             <button
